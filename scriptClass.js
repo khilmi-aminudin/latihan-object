b479771577ac6d662307311e4bb42674524715ff
khilmi aminudin <khilmiaminudin715@gmail.com>
2019-11-21 09:59:07 +0700

    membuat init file

---
+++ scriptClass.js
@@ -0,0 +1,29 @@
+
+// class Car {
+  
+//   constructor(type, color) {
+//     this.type =  type
+//     this.color = color
+//     this.engineStatus = 'off'
+//   }
+  
+//   engineStart () {
+    
+//     this.engineStatus = 'on'
+//     console.log('engine start')
+//   }
+  
+//   engingeStop () {
+//     this.engineStatus = 'off'
+//     console.log('engine start')
+//   }
+  
+//   static isEngineOn (car) {
+//     if (car.engineStatus === 'on') {
+//       console.log('Engine On')
+//     } else {
+//       console.log('Engine off')
+//     }
+//   }    
+  
+// }
---
+++ scriptObj.js
@@ -0,0 +1,48 @@
+//membuat object
+//object literal
+// let siswa = {
+//     nama    : 'khilmi aminudin',
+//     umur    : '21',
+//     asal    : 'Pemalang'  
+// };
+// console.log(siswa);
+
+//menhapus isi index 
+// let siswa = {
+//     nama    : 'khilmi aminudin',
+//     umur    : '21',
+//     asal    : 'Pemalang'  
+
+    //menggunakan "delete"
+    // delete siswa.nama
+    //delete siswa.umur 
+    //delete siswa.asal
+// };
+
+
+//method pada object
+
+// let barang = {
+//     nama    : "Televisi",
+//     tipe    : "14 inc",
+//     jenis   : "Elektronik",
+
+//     liat: function(){
+//         document.write("nama : "+barang.nama+"<br/> Tipe : "+
+//         barang.tipe+"<br/> jenis : "+barang.jenis);
+//     }
+
+// };
+
+// // barang.liat();
+
+// let motor = {
+//     merek   : "Yamaha",
+//     nama    : "Aerox",
+//     tahun   : "2018"
+
+// };
+
+
+
+
